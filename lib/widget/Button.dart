3ceab350532c862cc14792916f77dd8dbd4d1bc2
Sena Çelik <senaecelik@gmail.com>
2021-10-21 18:59:46 +0300

    calculator flutter

---
+++ lib/widget/Button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Button extends StatelessWidget {
+  Button({Key? key, required this.value, required this.funct})
+      : super(key: key);
+
+  final String value;
+  final Function funct;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+        child: Container(
+            alignment: Alignment.center,
+            width: 90,
+            height: 90,
+            child: Text(
+              value,
+              style: GoogleFonts.montserrat(
+                  fontSize: 30, fontWeight: FontWeight.w200),
+            )),
+        onPressed: () => value == '=' ? funct() : funct(value));
+  }
+}
